6381a3b62b26ddd0c9fed171e9a233b4356db958
bargest <bulgakovbarg@gmail.com>
2019-05-13 22:27:00 +0700

    Setup

---
+++ Source/Library/Public/Utility/MarchingCubesMeshComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProceduralMeshComponent.h"
+#include "MarchingCubesMeshComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIBRARY_API UMarchingCubesMeshComponent : public UProceduralMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
