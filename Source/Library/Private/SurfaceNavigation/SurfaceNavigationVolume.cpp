3bfd584b744ff10bf821cb49970eb0ab06dd23d3
bargest <bulgakovbarg@gmail.com>
2019-06-20 23:19:54 +0700

    Removed all shit from volume

--- Source/Library/Private/SurfaceNavigation/SurfaceNavigationVolume.cpp
+++ Source/Library/Private/SurfaceNavigation/SurfaceNavigationVolume.cpp
@@ -3,16 +3,10 @@
 #include "SurfaceNavigationVolume.h"
 #include "Engine/CollisionProfile.h"
 #include "Components/BoxComponent.h"
-#include "ProceduralMeshComponent.h"
-#include "DrawDebugHelpers.h"
-
-#include "SurfaceNavigation/SurfaceNavFunctionLibrary.h"
-#include "SurfaceNavigation/SurfaceNavLocalData.h"
-#include "MarchingCubesFunctionLibrary.h"
-#include "SurfaceNavigation/SurfaceNavBuilder.h"
 
 
-
+#include "SurfaceNavFunctionLibrary.h"
+#include "SurfaceNavigationSystem.h"
 
 
 
@@ -22,125 +16,75 @@ ASurfaceNavigationVolume::ASurfaceNavigationVolume()
 	Box->Mobility = EComponentMobility::Static;
 	Box->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
 	Box->SetCanEverAffectNavigation(false);
-	Box->SetBoxExtent(FVector(100));
+	Box->SetBoxExtent(FVector(100));	
 
 	RootComponent = Box;
 
-
-	Mesh = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("Mesh"));
-	Mesh->SetupAttachment(RootComponent);
-
-	static ConstructorHelpers::FObjectFinder<UMaterialInterface> Mat(TEXT("Material'/Game/Base/Materials/MTwoSided.MTwoSided'"));
-	if (Mat.Succeeded()) 
-	{
-		Mesh->SetMaterial(0, Mat.Object);
-	}
-
-	bShowMesh = true;
-	bShowGraph = true;
-	SurfaceValue = .5f;
+#if WITH_EDITORONLY_DATA
+	Status = TEXT("Add SurfaceNavigationSystem to the level");
+#endif
 }
 
+void ASurfaceNavigationVolume::PostRegisterAllComponents()
+{
+	Super::PostRegisterAllComponents();	
 
+	USurfaceNavigationSystem* NavSys = USurfaceNavFunctionLibrary::GetNavigationSystem(this);
+	if (GIsEditor && NavSys)
+	{
+		NavSys->VolumeAdded(this);
+	}
+
+#if WITH_EDITORONLY_DATA
+	Status = NavSys ? TEXT("Valid") : TEXT("SurfaceNavigationSystem is missing");
+#endif
+}
+
+void ASurfaceNavigationVolume::PostUnregisterAllComponents()
+{
+	Super::PostUnregisterAllComponents();
+
+	USurfaceNavigationSystem* NavSys = USurfaceNavFunctionLibrary::GetNavigationSystem(this);
+	if (GIsEditor && NavSys)
+	{
+		NavSys->VolumeRemoved(this);
+	}
+#if WITH_EDITORONLY_DATA
+	Status = NavSys ? TEXT("Valid") : TEXT("SurfaceNavigationSystem is missing");
+#endif
+}
+
+#if WITH_EDITOR
+void ASurfaceNavigationVolume::PostEditUndo()
+{
+	Super::PostEditUndo();
+	USurfaceNavigationSystem* NavSys = USurfaceNavFunctionLibrary::GetNavigationSystem(this);
+	if (GIsEditor && NavSys)
+	{
+		NavSys->VolumeUpdated(this);
+	}
+}
+
+void ASurfaceNavigationVolume::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	USurfaceNavigationSystem* NavSys = USurfaceNavFunctionLibrary::GetNavigationSystem(this);
+	if (GIsEditor && NavSys)
+	{
+		NavSys->VolumeUpdated(this);
+	}
+}
+#endif // WITH_EDITOR
 
 void ASurfaceNavigationVolume::OnConstruction(const FTransform& Transform)
 {
 	Super::OnConstruction(Transform);
 
-	CreateNavData();
-
-	FVector Location = NavData.ToLocation(NavData.FindClosestEdgeIndex(TestLoc));
-	if (FSurfaceNavigation::IsValidLocation(Location))
+	USurfaceNavigationSystem* NavSys = USurfaceNavFunctionLibrary::GetNavigationSystem(this);
+	if (GIsEditor && NavSys)
 	{
-		DrawDebugSphere(GetWorld(), GetActorTransform().TransformPosition(Location), 10, 12, FColor::Green, false, 15);
-	}
-}
-
-
-void ASurfaceNavigationVolume::ShowPath()
-{
-	TArray<FVector> path = NavData.FindPath(PathStart, TestLoc);
-
-	for (int Index = 0; Index < path.Num() - 1 ; Index++)
-	{
-		FVector p1 = GetActorTransform().TransformPosition(path[Index]);
-		FVector p2 = GetActorTransform().TransformPosition(path[Index+1]);
-		DrawDebugLine(GetWorld(), p1, p2, FColor::Green, false, 5, 0, 3);
-	}
-}
-
-void ASurfaceNavigationVolume::CreateNavData()
-{
-	Dimensions = FIntVector(GetBoxComponent()->GetScaledBoxExtent() * 2 / StepSize);
-	SurfaceValue = FMath::Clamp(SurfaceValue, 0.01f, 0.99f);
-
-	TArray<FVector4> Points;
-	SamplePoints(Dimensions, StepSize, Points);
-
-	FSurfaceNavBuilder Builder(NavData);
-	Builder.BuildGraph(Points, Dimensions, SurfaceValue);	
-	
-	SetupMeshFromPoints(Points, Dimensions);
-	if (bShowGraph) 
-	{
-		FlushPersistentDebugLines(GetWorld());
-		USurfaceNavFunctionLibrary::DrawSurfaceGraph(this, GetActorTransform(), NavData);
-	}
-}
-
-
-
-
-void ASurfaceNavigationVolume::SetupMeshFromPoints(const TArray<FVector4>& Points, const FIntVector& Dimensions)
-{
-	if (bShowMesh) 
-	{
-		const UWorld* World = GetWorld();
-		const FTransform ToWorld = GetActorTransform();
-
-		TArray<FVector> Vertices;
-		TArray<int32> Indices;
-
-		UMarchingCubesFunctionLibrary::GenerateMesh(Points, Dimensions, SurfaceValue, Vertices, Indices);
-		Mesh->CreateMeshSection(0, Vertices, Indices, {}, {}, {}, {}, {}, {}, {}, false);
-		Mesh->SetWorldScale3D(FVector(1));
-	}
-
-	Mesh->SetVisibility(bShowMesh);
-}
-
-void ASurfaceNavigationVolume::SamplePoints(const FIntVector& Dimensions, float CellSize, TArray<FVector4>& OutPoints) const
-{
-	const UWorld* World = GetWorld();
-
-	const FVector CellOffset = -GetBoxComponent()->GetScaledBoxExtent() + CellSize / 2;
-
-	const FTransform ToWorld = GetActorTransform();
-
-	const FCollisionShape TestBox = FCollisionShape::MakeBox(FVector(CellSize / 2));
-
-	FCollisionObjectQueryParams QueryParameters;
-	QueryParameters.AddObjectTypesToQuery(ECC_WorldStatic);
-	QueryParameters.AddObjectTypesToQuery(ECC_WorldDynamic);
-
-	FCollisionQueryParams Params;
-	Params.bFindInitialOverlaps = true;
-
-
-	OutPoints.Reset(Dimensions.X*Dimensions.Y*Dimensions.Z);
-	for (int Z = 0; Z < Dimensions.Z; Z++)
-	{
-		for (int Y = 0; Y < Dimensions.Y; Y++)
-		{
-			for (int X = 0; X < Dimensions.X; X++)
-			{
-				FVector TestLocation = FVector(X, Y, Z) * CellSize + CellOffset;
-				bool WasOverlap = World->OverlapAnyTestByObjectType(ToWorld.TransformPositionNoScale(TestLocation), ToWorld.GetRotation(), QueryParameters, TestBox, Params);
-
-				OutPoints.Add(FVector4(TestLocation, WasOverlap ? 1 : 0));
-			}
-		}
-	}
+		NavSys->VolumeUpdated(this);
+	}	
 }
 
 
@@ -148,29 +92,4 @@ void ASurfaceNavigationVolume::SamplePoints(const FIntVector& Dimensions, float 
 
 
 
-FIntVector ASurfaceNavigationVolume::ToCellCoordinates(const FVector& Location, bool WorldSpace /*= true*/) const
-{
-	FVector LocalPosition = WorldSpace ? GetActorTransform().InverseTransformPositionNoScale(Location) : Location;
 
-	return FIntVector((LocalPosition + GetBoxComponent()->GetUnscaledBoxExtent() - FVector(StepSize / 2)) / StepSize);
-}
-
-FVector ASurfaceNavigationVolume::ToWorldCoordinates(const FIntVector& CellCoordinates) const
-{
-	const FVector CellOffset = -GetBoxComponent()->GetScaledBoxExtent() + StepSize / 2;
-
-	FVector LocalLocation = FVector(CellCoordinates) * StepSize + CellOffset;
-
-	return GetActorTransform().TransformPositionNoScale(LocalLocation);
-}
-
-
-
-
-bool ASurfaceNavigationVolume::IsCoordinatesValid(const FIntVector& CellCoordinates)
-{
-	return
-		CellCoordinates.X >= 0 && CellCoordinates.X < Dimensions.X &&
-		CellCoordinates.Y >= 0 && CellCoordinates.Y < Dimensions.Y &&
-		CellCoordinates.Z >= 0 && CellCoordinates.Z < Dimensions.Z;
-}
--- Source/Library/Public/SurfaceNavigation/SurfaceNavigationVolume.h
+++ Source/Library/Public/SurfaceNavigation/SurfaceNavigationVolume.h
@@ -4,86 +4,43 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "SurfaceNavLocalData.h"
 #include "SurfaceNavigationVolume.generated.h"
 
 class UBoxComponent;
-class UProceduralMeshComponent;
-
-
-
 
 /**
- * 
+ * Basic SUrface Navigation volume
+ * Requires NavigationSystem actor in world to work
  */
-UCLASS(hidecategories = (Object, Display, Rendering, Physics, Input, Blueprint), ConversionRoot, NotBlueprintable)
+UCLASS(hidecategories = (Object, Display, Rendering, Physics, Input, Blueprint), autoExpandCategories=("Sampler"), ConversionRoot, NotBlueprintable)
 class LIBRARY_API ASurfaceNavigationVolume : public AActor
 {
 	GENERATED_BODY()
+	
+#if WITH_EDITORONLY_DATA	
+	UPROPERTY(VisibleAnywhere, Category = "SurfaceNavigation")
+	FString Status;
+#endif //  WITH_EDITORONLY_DATA
 
-	FSurfaceNavLocalData NavData;
-
-	UPROPERTY(VisibleAnywhere, Category=Collision, meta=(AllowPrivateAccess = "true"))
+	UPROPERTY(VisibleAnywhere, Category="SurfaceNavigation", meta=(AllowPrivateAccess = "true"))
 	UBoxComponent* Box;
 
-	UPROPERTY()
-	UProceduralMeshComponent* Mesh;
-
-public:
-
-	UPROPERTY(EditAnywhere, Category = "SurfaceNavigation")
-	bool bShowMesh;
-
-	UPROPERTY(EditAnywhere, Category = "SurfaceNavigation")
-	bool bShowGraph;
-
-	UPROPERTY(EditAnywhere, Category = "SurfaceNavigation")
-	float StepSize = 50;
-
-	UPROPERTY(EditAnywhere, Category = "SurfaceNavigation", meta=(ClampMin = 0.01, ClampMax = 0.99))
-	float SurfaceValue;
-
-	UPROPERTY(VisibleAnywhere, Category = "SurfaceNavigation")
-	FIntVector Dimensions;
-
-
-	UPROPERTY(VisibleAnywhere, Category = "SurfaceNavigationTest", meta=(MakeEditWidget))
-	FVector TestLoc;
-
-	UPROPERTY(VisibleAnywhere, Category = "SurfaceNavigationTest", meta = (MakeEditWidget))
-	FVector PathStart;
 public:
 
 	ASurfaceNavigationVolume();
 
+	virtual void PostRegisterAllComponents() override;
+
+	virtual void PostUnregisterAllComponents() override;
+
+#if WITH_EDITOR
+	virtual void PostEditUndo() override;
+
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif // WITH_EDITOR
+
 	virtual void OnConstruction(const FTransform& Transform) override;
 
-	UFUNCTION(BlueprintCallable, CallInEditor, Category = "SurfaceNavigation")
-		void ShowPath();
 
 	UBoxComponent* GetBoxComponent() const { return Box; }
-
-
-protected:
-	void CreateNavData();
-
-	void SetupMeshFromPoints(const TArray<FVector4>& Points, const FIntVector& Dimensions);
-
-	void SamplePoints(const FIntVector& Dimensions, float CellSize, TArray<FVector4>& OutPoints) const;
-
-	
-public:
-
-	// Interface functions
-
-
-	UFUNCTION(BlueprintCallable, Category = "SurfaceNavigation")
-	FIntVector ToCellCoordinates(const FVector& Location, bool WorldSpace = true) const;
-
-	UFUNCTION(BlueprintCallable, Category = "SurfaceNavigation")
-	FVector ToWorldCoordinates(const FIntVector& CellCoordinates) const;
-
-	UFUNCTION(BlueprintCallable, Category = "SurfaceNavigation")
-	bool IsCoordinatesValid(const FIntVector& CellCoordinates);
-
 };
